4679f5ace17a7d1415b6a66555a49904f9167660
Bartłomiej Gątarski <Bartomiej Gtarski>
2017-02-01 16:18:12 +0100

    update to work on server as .war

--- src/main/java/com/gatar/SpizarkaServletApplication.java
+++ src/main/java/com/gatar/SpizarkaServletApplication.java
@@ -2,9 +2,17 @@ package com.gatar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpizarkaServletApplication {
+public class SpizarkaServletApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpizarkaServletApplication.class);
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpizarkaServletApplication.class, args);
 	}
